b37700eb01cb0049439a1128e3f84f9284e7d123
Vyckou <vyckou@gmail.com>
2020-09-11 17:29:47 +0300

    removed Get-NewPSSessionOption function: * it is not used within script block

--- WindowsServiceManager/WindowsServiceManagerV4/DeployWindowsService.ps1
+++ WindowsServiceManager/WindowsServiceManagerV4/DeployWindowsService.ps1
@@ -147,22 +147,6 @@ $scriptBlock = {
     $startService = $args[14]
     $RecreateService = $args[15]
 
-    function Get-NewPSSessionOption {
-        [CmdletBinding()]
-        param(
-            [string] $arguments
-        )
-        Trace-VstsEnteringInvocation $MyInvocation
-        try {
-            $commandString = "New-PSSessionOption $arguments"
-            Write-Verbose "New-PSSessionOption command: $commandString"
-            return (Invoke-Expression -Command $commandString)
-        }
-        finally {
-            Trace-VstsLeavingInvocation $MyInvocation
-        }
-    }
-
     function Get-WindowsService {
         [CmdletBinding()]
         param   (
